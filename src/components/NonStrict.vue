88c80e7322eb23ab6e768569b660e0296a218df4
Ji ZHANG <zhangji87@gmail.com>
2018-04-14 23:02:16 +0800

    add fontawesome

--- src/components/NonStrict.vue
+++ src/components/NonStrict.vue
@@ -1,15 +1,51 @@
 <template>
-  <div>
-    <b-button variant="primary">Greeting</b-button>
+  <div class="page-non-script">
+    <b-form>
+      <b-form-group label="Table Name:">
+       <b-row>
+          <b-col cols="6">
+            <b-form-input v-model="table.table_name" required />
+          </b-col>
+        </b-row>
+      </b-form-group>
+      <b-form-group label="Category:">
+        <b-row>
+          <b-col cols="6">
+            <b-form-select v-model="table.category" :options="categoryOptions" />
+          </b-col>
+          <b-col cols="6">
+            <b-button variant="light">
+              <font-awesome-icon icon="plus" style="margin-right: 10px;" />Add Category
+            </b-button>
+          </b-col>
+        </b-row>
+      </b-form-group>
+      <b-button type="submit" variant="primary">Submit</b-button>
+    </b-form>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'NonStrict'
+  name: 'NonStrict',
+  data () {
+    return {
+      table: {
+        table_name: '',
+        category: 0
+      },
+      categoryOptions: [
+        { value: 0, text: 'Choose' },
+        { value: 1, text: 'Sales' },
+        { value: 2, text: 'Marketing' }
+      ]
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.page-non-script {
+  padding: 15px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -4,11 +4,17 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import BootstrapVue from 'bootstrap-vue'
+import fontawesome from '@fortawesome/fontawesome'
+import solid from '@fortawesome/fontawesome-free-solid'
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 Vue.use(BootstrapVue)
 Vue.config.productionTip = false
+
+fontawesome.library.add(solid)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 /* eslint-disable no-new */
 new Vue({
